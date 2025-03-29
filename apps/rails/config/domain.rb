7e452fdf32665707d4aacf20ae9136b527429340
Sumit Vekariya <sarkazein7@gmail.com>
2025-03-29 11:41:54 +0530

    Simplify domain.rb by directly using environment variables

--- apps/rails/config/domain.rb
+++ apps/rails/config/domain.rb
@@ -1,48 +1,8 @@
 # frozen_string_literal: true
 
-configuration_by_env = {
-  production: {
-    protocol: "https",
-    root_domain: ENV.fetch("ROOT_DOMAIN", "flexile.com"),
-    domain: ENV.fetch("APP_DOMAIN", "app.flexile.com"),
-    api_domain: ENV.fetch("API_DOMAIN", "api.flexile.com"),
-    email_domain: ENV.fetch("EMAIL_DOMAIN", "flexile.com"),
-  },
-  staging: {
-    protocol: "https",
-    root_domain: ENV.fetch("STAGING_ROOT_DOMAIN", "demo.flexile.com"),
-    domain: ENV.fetch("STAGING_APP_DOMAIN", "demo.flexile.com"),
-    api_domain: ENV.fetch("STAGING_API_DOMAIN", "api.demo.flexile.com"),
-    email_domain: ENV.fetch("STAGING_EMAIL_DOMAIN", "demo.flexile.com"),
-  },
-  test: {
-    protocol: "https",
-    root_domain: ENV.fetch("TEST_ROOT_DOMAIN", "test.flexile.dev"),
-    domain: ENV.fetch("TEST_APP_DOMAIN", "test.flexile.dev"),
-    api_domain: ENV.fetch("TEST_API_DOMAIN", "api.test.flexile.dev"),
-    email_domain: ENV.fetch("TEST_EMAIL_DOMAIN", "test.flexile.dev"),
-  },
-  development: {
-    protocol: "https",
-    root_domain: ENV.fetch("DEV_ROOT_DOMAIN", "flexile.dev"),
-    domain: ENV.fetch("DEV_APP_DOMAIN", "flexile.dev"),
-    api_domain: ENV.fetch("DEV_API_DOMAIN", "api.flexile.dev"),
-    email_domain: ENV.fetch("DEV_EMAIL_DOMAIN", "flexile.dev"),
-  },
-}
-
-config = configuration_by_env[Rails.env.to_sym]
-
-if Rails.env.development? && ENV["LOCAL_PROXY_DOMAIN"].present?
-  ROOT_DOMAIN = ENV["LOCAL_PROXY_DOMAIN"]
-  DOMAIN = ENV["LOCAL_PROXY_DOMAIN"]
-  API_DOMAIN = ENV["LOCAL_PROXY_DOMAIN"]
-  EMAIL_DOMAIN = configuration_by_env[:development][:email_domain]
-else
-  ROOT_DOMAIN = config[:root_domain]
-  DOMAIN = config[:domain]
-  API_DOMAIN = config[:api_domain]
-  EMAIL_DOMAIN = config[:email_domain]
-end
-
-PROTOCOL = config[:protocol]
+# Simple configuration that relies on environment variables being set appropriately for each environment
+PROTOCOL = "https"
+ROOT_DOMAIN = ENV.fetch("DOMAIN", "flexile.com")
+DOMAIN = ENV.fetch("APP_DOMAIN", "app.flexile.com")
+API_DOMAIN = ENV.fetch("API_DOMAIN", "api.flexile.com")
+EMAIL_DOMAIN = ENV.fetch("EMAIL_DOMAIN", "flexile.com")
