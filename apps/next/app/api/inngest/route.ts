1bc55bc26f55cc1b138bc5f76cba5466972d32d6
Lillian Rose <118373811+lillianrubyrose@users.noreply.github.com>
2025-04-09 18:47:54 -0400

    Update route.ts

--- apps/next/app/api/inngest/route.ts
+++ apps/next/app/api/inngest/route.ts
@@ -2,10 +2,15 @@ import { serve } from "inngest/next";
 import { inngest } from "@/inngest/client";
 import functions from "@/inngest/functions";
 
-export const maxDuration = 300;
+export const   maxDuration   =   300  ;
 
-export const { GET, POST, PUT } = serve({
-  client: inngest,
-  functions,
-  streaming: "allow",
+export   const {  GET,  POST ,  PUT  } = serve         (         { 
+  
+  client: inngest
+  
+  ,
+  functions   
+  ,     
+  
+     streaming: "allow",
 });
