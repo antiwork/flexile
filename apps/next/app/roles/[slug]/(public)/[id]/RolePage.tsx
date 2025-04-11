6dfa49f684748fb38469428ee0f770f352094291
Sushil Kumar <skdatyal@gmail.com>
2025-04-11 13:03:54 +0530

    refactor(ui): migrate notice to shadcn alert component

--- apps/next/app/roles/[slug]/(public)/[id]/RolePage.tsx
+++ apps/next/app/roles/[slug]/(public)/[id]/RolePage.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { LinkIcon } from "@heroicons/react/16/solid";
+import { ExclamationTriangleIcon } from "@heroicons/react/20/solid";
 import { CheckCircleIcon } from "@heroicons/react/24/outline";
 import { useMutation } from "@tanstack/react-query";
 import { Set } from "immutable";
@@ -13,10 +14,10 @@ import { Card, CardRow } from "@/components/Card";
 import Input from "@/components/Input";
 import SimpleLayout from "@/components/layouts/Simple";
 import MutationButton from "@/components/MutationButton";
-import Notice from "@/components/Notice";
 import RangeInput from "@/components/RangeInput";
 import RichText, { Editor as RichTextEditor } from "@/components/RichText";
 import Select from "@/components/Select";
+import { Alert, AlertDescription } from "@/components/ui/alert";
 import { DEFAULT_WORKING_HOURS_PER_WEEK, MAX_WORKING_HOURS_PER_WEEK, WORKING_WEEKS_PER_YEAR } from "@/models";
 import { countries, countryInfos } from "@/models/constants";
 import { PayRateType, trpc } from "@/trpc/client";
@@ -206,9 +207,12 @@ export default function RolePage({ countryCode }: { countryCode: string }) {
           </Card>
 
           {role.trialEnabled ? (
-            <Notice variant="critical">
-              This role has a trial period with a rate of {formatMoneyFromCents(role.trialPayRateInSubunits)} / hour.
-            </Notice>
+            <Alert variant="critical">
+              <ExclamationTriangleIcon />
+              <AlertDescription>
+                This role has a trial period with a rate of {formatMoneyFromCents(role.trialPayRateInSubunits)} / hour.
+              </AlertDescription>
+            </Alert>
           ) : null}
 
           <Button disabled={!role.activelyHiring} onClick={() => void setStep("applying")}>
@@ -263,9 +267,12 @@ export default function RolePage({ countryCode }: { countryCode: string }) {
               />
 
               {values.countryCode && isAdditionalSupportedCountry(values.countryCode) ? (
-                <Notice variant="critical">
-                  You'll need a bank account outside of {countries.get(values.countryCode)} to receive payments.
-                </Notice>
+                <Alert variant="critical">
+                  <ExclamationTriangleIcon />
+                  <AlertDescription>
+                    You'll need a bank account outside of {countries.get(values.countryCode)} to receive payments.
+                  </AlertDescription>
+                </Alert>
               ) : null}
 
               {role.payRateType === PayRateType.Hourly && (
