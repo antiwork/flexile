a420486ddf7d9f0adbb61a625139ab35f72eafd9
Jason Chang <jasonchang101@gmail.com>
2025-04-13 20:10:22 -0400

    f

--- apps/next/app/settings/page.tsx
+++ apps/next/app/settings/page.tsx
@@ -4,11 +4,13 @@ import { useUser } from "@clerk/nextjs";
 import { isClerkAPIResponseError } from "@clerk/nextjs/errors";
 import { useMutation } from "@tanstack/react-query";
 import { Map } from "immutable";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 import { CardRow } from "@/components/Card";
 import FormSection from "@/components/FormSection";
 import MutationButton from "@/components/MutationButton";
-import SimpleInput from "@/components/SimpleInput";
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import { useCurrentUser } from "@/global";
 import { MAX_PREFERRED_NAME_LENGTH, MIN_EMAIL_LENGTH } from "@/models";
 import { trpc } from "@/trpc/client";
@@ -27,14 +29,20 @@ export default function SettingsPage() {
 
 const DetailsSection = () => {
   const user = useCurrentUser();
-  const [email, setEmail] = useState(user.email);
-  const [preferredName, setPreferredName] = useState(user.preferredName || "");
+  const form = useForm({
+    defaultValues: {
+      email: user.email,
+      preferredName: user.preferredName || "",
+    },
+  });
+
   const saveMutation = trpc.users.update.useMutation();
   const handleSubmit = useMutation({
     mutationFn: async () => {
+      const values = form.getValues();
       await saveMutation.mutateAsync({
-        email,
-        preferredName,
+        email: values.email,
+        preferredName: values.preferredName,
       });
     },
     onSuccess: () => setTimeout(() => handleSubmit.reset(), 2000),
@@ -43,19 +51,32 @@ const DetailsSection = () => {
   return (
     <FormSection title="Personal details" onSubmit={e(() => handleSubmit.mutate(), "prevent")}>
       <CardRow className="grid gap-4">
-        <SimpleInput
-          label="Email"
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          minLength={MIN_EMAIL_LENGTH}
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input type="email" minLength={MIN_EMAIL_LENGTH} {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
         />
-        <SimpleInput
-          label="Preferred name (visible to others)"
-          placeholder="Enter preferred name"
-          maxLength={MAX_PREFERRED_NAME_LENGTH}
-          value={preferredName}
-          onChange={(e) => setPreferredName(e.target.value)}
+
+        <FormField
+          control={form.control}
+          name="preferredName"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Preferred name (visible to others)</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter preferred name" maxLength={MAX_PREFERRED_NAME_LENGTH} {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
         />
       </CardRow>
       <CardRow>
@@ -69,29 +90,33 @@ const DetailsSection = () => {
 
 const PasswordSection = () => {
   const { user } = useUser();
-  const [currentPassword, setCurrentPassword] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const form = useForm({
+    defaultValues: {
+      currentPassword: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
   const [errors, setErrors] = useState(Map<string, string>());
-  const data = { currentPassword, password, confirmPassword };
-  Object.entries(data).forEach(([key, value]) => useEffect(() => setErrors(errors.delete(key)), [value]));
 
   const saveMutation = useMutation({
     mutationFn: async () => {
+      const values = form.getValues();
       const newErrors = errors.clear().withMutations((errors) => {
-        Object.entries(data).forEach(([key, value]) => {
+        Object.entries(values).forEach(([key, value]) => {
           if (!value) errors.set(key, "This field is required.");
         });
-        if (data.password !== data.confirmPassword) errors.set("confirm_password", "Passwords do not match.");
+        if (values.password !== values.confirmPassword) errors.set("confirm_password", "Passwords do not match.");
       });
 
       setErrors(newErrors);
       if (newErrors.size > 0) return;
       try {
-        await assertDefined(user).updatePassword({ currentPassword, newPassword: password });
-        setCurrentPassword("");
-        setPassword("");
-        setConfirmPassword("");
+        await assertDefined(user).updatePassword({
+          currentPassword: values.currentPassword,
+          newPassword: values.password,
+        });
+        form.reset();
       } catch (error) {
         if (!isClerkAPIResponseError(error)) throw error;
         setErrors(errors.set("password", error.message));
@@ -104,45 +129,57 @@ const PasswordSection = () => {
   return (
     <FormSection title="Password" onSubmit={e(() => saveMutation.mutate(), "prevent")}>
       <CardRow className="grid gap-4">
-        <SimpleInput
-          label="Old password"
-          type="password"
-          value={currentPassword}
-          onChange={(e) => setCurrentPassword(e.target.value)}
-          aria-invalid={errors.has("current_password")}
-          aria-describedby={errors.has("current_password") ? "current_password_error" : undefined}
+        <FormField
+          control={form.control}
+          name="currentPassword"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Old password</FormLabel>
+              <FormControl>
+                <Input
+                  type="password"
+                  invalid={errors.has("current_password")}
+                  help={errors.get("current_password")}
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
         />
-        {errors.has("current_password") && (
-          <p id="current_password_error" className="text-destructive text-sm">
-            {errors.get("current_password")}
-          </p>
-        )}
-        <SimpleInput
-          label="New password"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          aria-invalid={errors.has("password")}
-          aria-describedby={errors.has("password") ? "password_error" : undefined}
+
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>New password</FormLabel>
+              <FormControl>
+                <Input type="password" invalid={errors.has("password")} help={errors.get("password")} {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
         />
-        {errors.has("password") && (
-          <p id="password_error" className="text-destructive text-sm">
-            {errors.get("password")}
-          </p>
-        )}
-        <SimpleInput
-          label="Confirm new password"
-          type="password"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          aria-invalid={errors.has("confirm_password")}
-          aria-describedby={errors.has("confirm_password") ? "confirm_password_error" : undefined}
+
+        <FormField
+          control={form.control}
+          name="confirmPassword"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Confirm new password</FormLabel>
+              <FormControl>
+                <Input
+                  type="password"
+                  invalid={errors.has("confirm_password")}
+                  help={errors.get("confirm_password")}
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
         />
-        {errors.has("confirm_password") && (
-          <p id="confirm_password_error" className="text-destructive text-sm">
-            {errors.get("confirm_password")}
-          </p>
-        )}
       </CardRow>
       <CardRow>
         <MutationButton type="submit" mutation={saveMutation} loadingText="Saving...">
