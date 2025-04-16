e9c471f5a75e617c614d6fd92e43ad8423f54460
Sushil Kumar <skdatyal@gmail.com>
2025-04-16 15:59:37 +0530

    fix: resolve comments

--- apps/next/components/ui/divider.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import { cn } from "@/utils";
-
-function HorizontalDivider({ className, omit = false, ...props }: React.ComponentProps<"div"> & { omit?: boolean }) {
-  return omit ? null : <div data-slot="horizontal-divider" className={cn("my-4 border-b", className)} {...props} />;
-}
-
-export { HorizontalDivider };
