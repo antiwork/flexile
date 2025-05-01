9d09406d26765761e05ba69e6bbf2317ea5d5b55
jc26 <jasonchang101@gmail.com>
2025-05-01 12:20:39 -0400

    f

--- apps/next/components/ui/input.tsx
+++ apps/next/components/ui/input.tsx
@@ -1,20 +1,22 @@
 import * as React from "react";
 import { cn } from "@/utils/index";
 
-type InputProps = React.ComponentProps<"input">;
-function Input({
-  className,
-  type,
-  value,
-  ...props
-}: Omit<InputProps, "value"> & { value?: InputProps["value"] | null }) {
+type InputProps = Omit<React.ComponentProps<"input">, "value" | "onChange"> & {
+  value?: React.ComponentProps<"input">["value"] | null;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+};
+
+function Input({ className, type, value, ...props }: InputProps) {
   return (
     <input
       type={type}
-      value={value ?? ""}
+      value={type !== "file" ? (value ?? "") : undefined}
+      onChange={props.onChange}
       data-slot="input"
       className={cn(
-        "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50",
+        "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 items-center rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:font-normal disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50",
+        type === "file" &&
+          "file:border-muted flex p-0 pe-3 file:me-3 file:h-full file:items-center file:border-e file:px-3 file:font-normal",
         "focus-visible:border-ring focus-visible:ring-ring/15 focus-visible:ring-[3px]",
         "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
         className,
