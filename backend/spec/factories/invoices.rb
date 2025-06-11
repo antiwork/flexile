fadb865168fafe330aca5742d1a9f3e8a9529acd
Maya <19721695+code-elf@users.noreply.github.com>
2025-06-12 01:04:24 +0200

    fix

--- backend/spec/factories/invoices.rb
+++ backend/spec/factories/invoices.rb
@@ -23,7 +23,7 @@ FactoryBot.define do
     country_code { "US" }
 
     after :build do |invoice|
-      invoice.invoice_line_items << build(:invoice_line_item, invoice: nil, minutes: 60)
+      invoice.invoice_line_items << build(:invoice_line_item, invoice: nil, quantity: 1)
       invoice.flexile_fee_cents ||= invoice.calculate_flexile_fee_cents
     end
 
