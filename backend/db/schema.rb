6cecfbd8cf6e4950c7446096f787ca90620144a2
Maya <19721695+code-elf@users.noreply.github.com>
2025-06-15 00:43:56 +0200

    fix

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_12_194925) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_15_204642) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
