87cdd12f349a02cc0ba85dec3aab9268507be601
BeNikk <bhattnik442@gmail.com>
2025-07-04 20:59:53 +0530

    refactor:replace useSuspenseQuery with useQuery to show skeleton

--- frontend/app/people/page.tsx
+++ frontend/app/people/page.tsx
@@ -27,6 +27,7 @@ import TemplateSelector from "@/app/document_templates/TemplateSelector";
 import FormFields from "./FormFields";
 import { DEFAULT_WORKING_HOURS_PER_WEEK } from "@/models";
 import { Switch } from "@/components/ui/switch";
+import TableSkeleton from "@/components/TableSkeleton";
 
 const schema = z.object({
   email: z.string().email(),
@@ -42,7 +43,7 @@ const schema = z.object({
 export default function PeoplePage() {
   const company = useCurrentCompany();
   const router = useRouter();
-  const [workers, { refetch }] = trpc.contractors.list.useSuspenseQuery({ companyId: company.id });
+  const { data: workers = [], isLoading, refetch } = trpc.contractors.list.useQuery({ companyId: company.id });
   const [showInviteModal, setShowInviteModal] = useState(false);
   const lastContractor = workers[0];
 
@@ -138,7 +139,9 @@ export default function PeoplePage() {
         ) : null
       }
     >
-      {workers.length > 0 ? (
+      {isLoading ? (
+        <TableSkeleton />
+      ) : workers.length > 0 ? (
         <DataTable
           table={table}
           searchColumn="user_name"
