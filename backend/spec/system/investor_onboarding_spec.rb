1aeeb577b8a0d6de0ffc55cfa85b0cef2d7d34d5
dniteng <dniteng@outlook.com>
2025-07-21 12:51:44 +0700

    chore: restore the backend/spec/system/investor_onboarding_spec.rb to remove it in another PR

--- backend/spec/system/investor_onboarding_spec.rb
+++ backend/spec/system/investor_onboarding_spec.rb
@@ -27,12 +27,50 @@ RSpec.describe "Onboarding for a user with investor role", :vcr do
     select "Australia", from: "Country of citizenship"
   end
 
+  def fill_in_legal_details
+    expect(page).to have_current_path(spa_company_investor_onboarding_legal_path(company.external_id))
+    expect(page).to have_selector("h1", text: "What's your legal entity?")
+    fill_in user.reload.requires_w9? ? "Tax identification number (SSN or ITIN)" : "Foreign tax identification number", with: "123-45-6789"
+    fill_in "Date of birth", with: "06/07/1980"
+    fill_in "Residential address (street name, number, apartment)", with: "123 Main St"
+    fill_in "City", with: "New York"
+    fill_in zip_code_label, with: "10001"
+  end
+
   it "uses the investor flow for onboarding" do
     visit root_path
 
     fill_in_personal_details
     click_on "Continue"
 
+    fill_in_legal_details
+    select "New York", from: "State"
+    wait_for_ajax
+    click_on "Continue"
+    within_modal do
+      click_on "Save"
+    end
+
+    click_on "Set up"
+    select_wise_field "USD (United States Dollar)", from: "Currency"
+    expect(page).to have_field("Full name of the account holder", with: legal_name)
+    fill_in "ACH routing number", with: "026009593"
+    fill_in "Account number", with: "12345678"
+    within_modal do
+      click_on "Continue"
+    end
+    select_wise_field "United States", from: "Country"
+    fill_in "City", with: "San Francisco"
+    fill_in "Street address, apt number", with: "59-720 Kamehameha Hwy"
+    select "Hawaii", from: "State"
+    fill_in "ZIP code", with: "96712"
+
+    click_on "Save bank account"
+    wait_for_ajax
+
+    expect(page).to have_text("Account ending in 5678")
+
+    click_on "Continue"
     expect(page).to have_current_path(spa_company_dividends_path(company.external_id))
     expect(page).to have_text("Equity")
   end
@@ -48,6 +86,13 @@ RSpec.describe "Onboarding for a user with investor role", :vcr do
         expect(page).to have_text("Unfortunately, due to regulatory restrictions and compliance with international sanctions, individuals from sanctioned countries are unable to receive payments through our platform.")
         expect(page).to have_text("You can still use Flexile's features such as receiving equity, but you won't be able to set a payout method or receive any payments.")
         click_on "Proceed"
+      end
+
+      fill_in_legal_details
+      select "Pinar del Río", from: "State"
+      click_on "Continue"
+      within_modal do
+        click_on "Save"
       end
 
       # Skips the bank account step
