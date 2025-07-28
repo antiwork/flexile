1a4640508c145b610aa43b3d4f52c835f0a4f0f7
Ershad Kunnakkadan <ershad92@gmail.com>
2025-07-28 17:49:31 -0400

    Refactor

--- backend/spec/models/concerns/user/otp_authentication_spec.rb
+++ backend/spec/models/concerns/user/otp_authentication_spec.rb
@@ -8,7 +8,6 @@ RSpec.describe User::OtpAuthentication, type: :model do
   describe "#verify_otp" do
     context "when in test environment with PLAYWRIGHT_TEST=true" do
       before do
-        allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new("test"))
         ENV["PLAYWRIGHT_TEST"] = "true"
       end
 
@@ -21,11 +20,7 @@ RSpec.describe User::OtpAuthentication, type: :model do
       end
 
       it "still validates normal OTP codes" do
-        # Mock the normal OTP validation
-        allow(user).to receive(:authenticate_otp).and_return(true)
-
-        expect(user.verify_otp("123456")).to be true
-        expect(user).to have_received(:authenticate_otp).with("123456", drift: User::OtpAuthentication::OTP_DRIFT)
+        expect(user.verify_otp(user.otp_code)).to be true
       end
     end
 
@@ -40,10 +35,7 @@ RSpec.describe User::OtpAuthentication, type: :model do
       end
 
       it 'does not accept "000000" as valid OTP code' do
-        allow(user).to receive(:authenticate_otp).and_return(false)
-
         expect(user.verify_otp("000000")).to be false
-        expect(user).to have_received(:authenticate_otp).with("000000", drift: User::OtpAuthentication::OTP_DRIFT)
       end
     end
 
@@ -54,10 +46,7 @@ RSpec.describe User::OtpAuthentication, type: :model do
       end
 
       it 'does not accept "000000" as valid OTP code' do
-        allow(user).to receive(:authenticate_otp).and_return(false)
-
         expect(user.verify_otp("000000")).to be false
-        expect(user).to have_received(:authenticate_otp).with("000000", drift: User::OtpAuthentication::OTP_DRIFT)
       end
     end
   end
