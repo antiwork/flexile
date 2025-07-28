4a5b2274ff8cddb1922fad90100d04cdac50f64c
Anshgrover23 <ag5989670@gmail.com>
2025-07-29 01:22:58 +0530

    refactor: consolidate equity flags into equity_enabled

--- backend/spec/system/company/dividend_rounds/listing_page_spec.rb
+++ backend/spec/system/company/dividend_rounds/listing_page_spec.rb
@@ -53,7 +53,7 @@ RSpec.describe "Dividend Rounds listing page" do
     end
 
     it "shows the 'Grants' tab when the relevant feature is enabled" do
-      company.update!(equity_grants_enabled: true)
+      company.update!(equity_enabled: true)
       sign_in user
 
       visit spa_company_dividend_rounds_path(company.external_id)
