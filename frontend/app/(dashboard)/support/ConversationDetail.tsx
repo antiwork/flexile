cc6267b954e927d9ba29762f273b4b345f63190d
Sahil Lavingia <sahil.lavingia@gmail.com>
2025-07-31 17:33:21 -0400

    Update ConversationDetail.tsx

--- frontend/app/(dashboard)/support/ConversationDetail.tsx
+++ frontend/app/(dashboard)/support/ConversationDetail.tsx
@@ -4,13 +4,6 @@ import { useConversation } from "@helperai/react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { DashboardHeader } from "@/components/DashboardHeader";
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb";
 import { HelperChat } from "./HelperChat";
 
 interface ConversationDetailProps {
@@ -29,19 +22,7 @@ export const ConversationDetail = ({ conversationSlug }: ConversationDetailProps
   return (
     <>
       <DashboardHeader
-        title={
-          <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem>
-                <Link href="/support">Support center</Link>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator />
-              <BreadcrumbItem>
-                <BreadcrumbPage>{conversation?.subject}</BreadcrumbPage>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
-        }
+        title={conversation?.subject}
       />
       {conversation ? <HelperChat conversation={conversation} /> : null}
     </>
