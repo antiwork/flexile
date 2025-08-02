da55bf32981b19d21e2513df9c5b97fb8b64d29a
naaa760 <neh6a683@gmail.com>
2025-08-03 00:22:06 +0530

    rmoved Clerk dependencies

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
-require "clerk/authenticatable"
-
 class ApplicationController < ActionController::Base
-  include Clerk::Authenticatable
   include PunditAuthorization, SetCurrent
 
   before_action :set_paper_trail_whodunnit
