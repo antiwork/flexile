3ef1f6884247e671000c00da85030553808f9a7c
Sahil Lavingia <sahil.lavingia@gmail.com>
2025-08-04 16:11:17 -0400

    cp

--- backend/db/migrate/20240522184215_remove_board_consents_and_members.rb
+++ backend/db/migrate/20240522184215_remove_board_consents_and_members.rb
@@ -1,7 +1,7 @@
 class RemoveBoardConsentsAndMembers < ActiveRecord::Migration[8.0]
   def change
-    drop_table :board_consents, if_exists: true
-    remove_column :company_administrators, :board_member, if_exists: true
+    drop_table :board_consents
+    remove_column :company_administrators, :board_member
     drop_enum :board_consent_status
   end
 end
