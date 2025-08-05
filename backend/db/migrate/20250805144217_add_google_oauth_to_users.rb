a874be78930743bfe42a83c2c8b6e17a86975f4e
andev18 <andev18fullstack@gmail.com>
2025-08-05 18:01:20 +0700

    fix: polish code

--- backend/db/migrate/20250805144217_add_google_oauth_to_users.rb
+++ backend/db/migrate/20250805144217_add_google_oauth_to_users.rb
@@ -1,7 +1,6 @@
 class AddGoogleOauthToUsers < ActiveRecord::Migration[7.1]
   def change
     add_column :users, :google_uid, :string
-    add_column :users, :avatar_url, :string
     add_index :users, :google_uid, unique: true
   end
 end
--- backend/db/migrate/20250805144218_remove_avatar_url_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAvatarUrlFromUsers < ActiveRecord::Migration[8.0]
-  def change
-    remove_column :users, :avatar_url, :string
-  end
-end
