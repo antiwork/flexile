cf72a4733f6336937bb380a3976ae40213c8f9cf
nazar <gladyshinbox@outlook.com>
2025-08-07 10:43:12 +0200

    delete migrations

--- backend/db/migrate/20250804204138_add_name_to_dividend_computations.rb
+++
@@ -1,5 +0,0 @@
-class AddNameToDividendComputations < ActiveRecord::Migration[8.0]
-  def change
-    add_column :dividend_computations, :name, :string
-  end
-end
--- backend/db/migrate/20250804205026_make_name_required_in_dividend_computations.rb
+++
@@ -1,9 +0,0 @@
-class MakeNameRequiredInDividendComputations < ActiveRecord::Migration[8.0]
-  def change
-    # First, update any existing records with null names to have a default value
-    execute "UPDATE dividend_computations SET name = 'Distribution' WHERE name IS NULL"
-
-    # Then make the column NOT NULL
-    change_column_null :dividend_computations, :name, false
-  end
-end
