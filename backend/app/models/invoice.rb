b16fdd85ce6755a3080ac5d729fd80098375db0d
nazar <gladyshinbox@outlook.com>
2025-08-12 18:24:45 +0200

    address feedback

--- backend/app/models/invoice.rb
+++ backend/app/models/invoice.rb
@@ -29,10 +29,7 @@ class Invoice < ApplicationRecord
   DELETABLE_STATES = [RECEIVED, APPROVED]
   ALL_STATES = READ_ONLY_STATES + EDITABLE_STATES
 
-  BASE_FLEXILE_FEE_CENTS = 50
-  MAX_FLEXILE_FEE_CENTS = 15_00
-  PERCENT_FLEXILE_FEE = 1.5
-  private_constant :BASE_FLEXILE_FEE_CENTS, :MAX_FLEXILE_FEE_CENTS, :PERCENT_FLEXILE_FEE
+
 
   has_many :invoice_line_items, autosave: true
   has_many :invoice_expenses, autosave: true
@@ -83,7 +80,7 @@ class Invoice < ApplicationRecord
   }
   validates :flexile_fee_cents, presence: true, numericality: {
     only_integer: true,
-    greater_than_or_equal_to: BASE_FLEXILE_FEE_CENTS,
+    greater_than_or_equal_to: FlexileFeeCalculator::INVOICE_BASE_FEE_CENTS,
   }, on: :create
   validate :total_must_be_a_sum_of_cash_and_equity
   validate :min_equity_less_than_max_equity
--- backend/app/services/flexile_fee_calculator.rb
+++ backend/app/services/flexile_fee_calculator.rb
@@ -10,20 +10,12 @@ class FlexileFeeCalculator
   DIVIDEND_MAX_FEE_CENTS = 30_00
 
   def self.calculate_invoice_fee_cents(total_amount_in_usd_cents)
-    new.calculate_invoice_fee_cents(total_amount_in_usd_cents)
-  end
-
-  def self.calculate_dividend_fee_cents(total_amount_in_cents)
-    new.calculate_dividend_fee_cents(total_amount_in_cents)
-  end
-
-  def calculate_invoice_fee_cents(total_amount_in_usd_cents)
     percentage_fee_cents = (total_amount_in_usd_cents.to_d * INVOICE_PERCENTAGE.to_d / 100).round
     fee_cents = INVOICE_BASE_FEE_CENTS + percentage_fee_cents
     [fee_cents, INVOICE_MAX_FEE_CENTS].min
   end
 
-  def calculate_dividend_fee_cents(total_amount_in_cents)
+  def self.calculate_dividend_fee_cents(total_amount_in_cents)
     percentage_fee_cents = (total_amount_in_cents.to_d * DIVIDEND_PERCENTAGE.to_d / 100).round
     fee_cents = DIVIDEND_BASE_FEE_CENTS + percentage_fee_cents
     [fee_cents, DIVIDEND_MAX_FEE_CENTS].min
