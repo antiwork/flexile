1df7b21faad2a65b1281f5b511df36de61b568ba
Smit <smitp.contact@gmail.com>
2025-08-13 15:17:28 +0530

    refactor(QuantityInput): save input field state on everykeystroke

--- frontend/app/(dashboard)/invoices/QuantityInput.tsx
+++ frontend/app/(dashboard)/invoices/QuantityInput.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Input } from "@/components/ui/input";
 import { formatDuration } from "@/utils/time";
 
@@ -13,31 +13,45 @@ const QuantityInput = ({
   onChange: (value: Value) => void;
 } & Omit<React.ComponentProps<"input">, "value" | "onChange">) => {
   const [rawValue, setRawValue] = useState("");
-  useEffect(
-    () => setRawValue(value ? (value.hourly ? formatDuration(value.quantity) : value.quantity.toString()) : ""),
-    [value],
+
+  const parseRawValue = useCallback((raw: string): Value => {
+    if (!raw.length) return null;
+    const valueSplit = raw.split(":");
+    if (valueSplit.length === 1) return { quantity: parseFloat(valueSplit[0] ?? "0"), hourly: false };
+    const hours = parseInt(valueSplit[0] ?? "0", 10);
+    const minutes = parseInt(valueSplit[1] ?? "0", 10);
+    return {
+      quantity: Math.floor(isNaN(hours) ? 0 : hours * 60) + (isNaN(minutes) ? 0 : minutes),
+      hourly: true,
+    };
+  }, []);
+
+  const onChangeLocal = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const newRawValue = e.target.value;
+      setRawValue(newRawValue);
+      const parsedValue = parseRawValue(newRawValue);
+      onChange(parsedValue);
+    },
+    [parseRawValue, onChange],
   );
 
-  return (
-    <Input
-      {...props}
-      value={rawValue}
-      onChange={(e) => setRawValue(e.target.value)}
-      onBlur={() => {
-        if (!rawValue.length) return onChange(null);
+  useEffect(() => {
+    const currentParsedValue = parseRawValue(rawValue);
 
-        const valueSplit = rawValue.split(":");
-        if (valueSplit.length === 1) return onChange({ quantity: parseFloat(valueSplit[0] ?? "0"), hourly: false });
+    const valuesEqual =
+      (currentParsedValue === null && value === null) ||
+      (currentParsedValue !== null &&
+        value !== null &&
+        currentParsedValue.quantity === value.quantity &&
+        currentParsedValue.hourly === value.hourly);
 
-        const hours = parseFloat(valueSplit[0] ?? "0");
-        const minutes = parseFloat(valueSplit[1] ?? "0");
-        onChange({
-          quantity: Math.floor(isNaN(hours) ? 0 : hours * 60) + (isNaN(minutes) ? 0 : minutes),
-          hourly: true,
-        });
-      }}
-    />
-  );
+    if (!valuesEqual) {
+      setRawValue(value ? (value.hourly ? formatDuration(value.quantity) : value.quantity.toString()) : "");
+    }
+  }, [value, rawValue, parseRawValue]);
+
+  return <Input {...props} value={rawValue} onChange={onChangeLocal} />;
 };
 
 export default QuantityInput;
