4d1febd5e11ddfcea469f9a24b7ea7309b6cc0a4
nazar <gladyshinbox@outlook.com>
2025-08-14 01:02:40 +0200

    nit

--- frontend/app/(dashboard)/equity/dividend_rounds/[type]/[id]/page.tsx
+++ frontend/app/(dashboard)/equity/dividend_rounds/[type]/[id]/page.tsx
@@ -81,7 +81,7 @@ const DividendRound = ({ id }: { id: string }) => {
   });
 
   const onRowClicked = (row: Dividend) => {
-    if (row.investor?.user?.id) {
+    if (row.investor.user.id) {
       router.push(`/people/${row.investor.user.id}?tab=shares`);
     }
   };
