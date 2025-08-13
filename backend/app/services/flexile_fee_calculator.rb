3cba0d32b3215d1f0c1a5e406b12f8cb3ec26aa6
nazar <gladyshinbox@outlook.com>
2025-08-13 16:34:35 +0200

    to_d

--- backend/app/services/flexile_fee_calculator.rb
+++ backend/app/services/flexile_fee_calculator.rb
@@ -10,13 +10,13 @@ class FlexileFeeCalculator
   DIVIDEND_MAX_FEE_CENTS = 30_00
 
   def self.calculate_invoice_fee_cents(total_amount_in_usd_cents)
-    percentage_fee_cents = (total_amount_in_usd_cents.to_d * INVOICE_PERCENTAGE.to_d / 100).round
+    percentage_fee_cents = (total_amount_in_usd_cents * INVOICE_PERCENTAGE / 100).round
     fee_cents = INVOICE_BASE_FEE_CENTS + percentage_fee_cents
     [fee_cents, INVOICE_MAX_FEE_CENTS].min
   end
 
   def self.calculate_dividend_fee_cents(total_amount_in_cents)
-    percentage_fee_cents = (total_amount_in_cents.to_d * DIVIDEND_PERCENTAGE.to_d / 100).round
+    percentage_fee_cents = (total_amount_in_cents * DIVIDEND_PERCENTAGE / 100).round
     fee_cents = DIVIDEND_BASE_FEE_CENTS + percentage_fee_cents
     [fee_cents, DIVIDEND_MAX_FEE_CENTS].min
   end
