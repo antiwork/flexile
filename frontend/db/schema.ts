5ec5bab3d728e9361e4c9c9426b436433f96671c
andev18 <andev18fullstack@gmail.com>
2025-08-14 10:50:26 +0700

    fix: remove redundant change

--- frontend/db/schema.ts
+++ frontend/db/schema.ts
@@ -1698,7 +1698,6 @@ export const users = pgTable(
     sentInvalidTaxIdEmail: boolean("sent_invalid_tax_id_email").notNull().default(false),
     clerkId: varchar("clerk_id"),
     otpSecretKey: varchar("otp_secret_key"),
-    googleUid: varchar("google_uid"),
   },
   (table) => [
     index("index_users_on_confirmation_token").using("btree", table.confirmationToken.asc().nullsLast().op("text_ops")),
