3b324ad4a628582d5f44b2774b19b10d0ffc29e2
nazar <gladyshinbox@outlook.com>
2025-08-16 17:29:45 +0200

    spec update

--- e2e/tests/company/equity/dividend-computations.spec.ts
+++ e2e/tests/company/equity/dividend-computations.spec.ts
@@ -1,11 +1,7 @@
 import { getLocalTimeZone, today } from "@internationalized/date";
 import { db, takeOrThrow } from "@test/db";
 import { companiesFactory } from "@test/factories/companies";
-import { companyInvestorsFactory } from "@test/factories/companyInvestors";
-import { dividendRoundsFactory } from "@test/factories/dividendRounds";
-import { shareClassesFactory } from "@test/factories/shareClasses";
-import { shareHoldingsFactory } from "@test/factories/shareHoldings";
-import { usersFactory } from "@test/factories/users";
+import { dividendComputationsFactory } from "@test/factories/dividendComputations";
 import { fillDatePicker } from "@test/helpers";
 import { login } from "@test/helpers/auth";
 import { expect, test, withinModal } from "@test/index";
@@ -15,63 +11,11 @@ import { dividendComputations } from "@/db/schema";
 import { formatDate } from "@/utils/time";
 
 test.describe("Dividend Computations", () => {
-  const setup = async () => {
+  test("creates dividend computation", async ({ page }) => {
     const { company, adminUser } = await companiesFactory.createCompletedOnboarding({
       equityEnabled: true,
     });
-
-    const { user: investorUser1 } = await usersFactory.create();
-    const { user: investorUser2 } = await usersFactory.create();
-
-    const { companyInvestor: investor1 } = await companyInvestorsFactory.create({
-      companyId: company.id,
-      userId: investorUser1.id,
-      investmentAmountInCents: 100000n,
-    });
-
-    const { companyInvestor: investor2 } = await companyInvestorsFactory.create({
-      companyId: company.id,
-      userId: investorUser2.id,
-      investmentAmountInCents: 200000n,
-    });
-
-    const { shareClass } = await shareClassesFactory.create({
-      companyId: company.id,
-      name: "Common",
-      originalIssuePriceInDollars: "10.00",
-    });
-
-    await shareHoldingsFactory.create({
-      companyInvestorId: investor1.id,
-      shareClassId: shareClass.id,
-      numberOfShares: 1000,
-      totalAmountInCents: 10000n,
-      sharePriceUsd: "10.00",
-    });
-
-    await shareHoldingsFactory.create({
-      companyInvestorId: investor2.id,
-      shareClassId: shareClass.id,
-      numberOfShares: 2000,
-      totalAmountInCents: 20000n,
-      sharePriceUsd: "10.00",
-    });
-
-    await dividendRoundsFactory.create({
-      companyId: company.id,
-      totalAmountInCents: 50000n,
-      numberOfShares: 1500n,
-      numberOfShareholders: 2n,
-      status: "Issued",
-      returnOfCapital: false,
-      readyForPayment: false,
-    });
-
-    return { company, adminUser, investor1, investor2, shareClass };
-  };
-
-  test("creates dividend computation", async ({ page }) => {
-    const { company, adminUser } = await setup();
+    await dividendComputationsFactory.setupInvestorsWithShareHoldings(company.id);
     const date = today(getLocalTimeZone()).add({ days: 15 });
 
     await login(page, adminUser);
@@ -122,7 +66,9 @@ test.describe("Dividend Computations", () => {
   });
 
   test("prevents creating dividend computation with date less than 10 days in future", async ({ page }) => {
-    const { adminUser } = await setup();
+    const { adminUser } = await companiesFactory.createCompletedOnboarding({
+      equityEnabled: true,
+    });
     const date = today(getLocalTimeZone()).add({ days: 5 });
 
     await login(page, adminUser);
