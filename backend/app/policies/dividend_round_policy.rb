d4a8b9e1c9e5f8f4fd4c036253fc264f19d04b63
nazar <gladyshinbox@outlook.com>
2025-08-18 23:23:47 +0200

    update policy

--- backend/app/policies/dividend_round_policy.rb
+++ backend/app/policies/dividend_round_policy.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 
 class DividendRoundPolicy < ApplicationPolicy
+  def index?
+    return unless company.equity_enabled?
+
+    company_administrator.present? || company_lawyer.present?
+  end
+
   def create?
     return false unless company.equity_enabled?
-    return false unless company_administrator.present?
 
-    true
+    company_administrator.present?
   end
 end
