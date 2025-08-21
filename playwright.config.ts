0d122694bce1a96f6752875d7c7aa6b0bd8454fc
Maya <19721695+code-elf@users.noreply.github.com>
2025-08-21 18:46:37 +0200

    fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
   testDir: "./e2e",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 1 : 0,
   workers: process.env.CI ? "100%" : undefined,
   reporter: process.env.CI ? [["list"], ["html"]] : "list",
   use: {
