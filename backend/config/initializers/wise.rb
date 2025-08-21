85ef00b7d2a5323ba5ce349bb2aee0442294cf54
Maya <19721695+code-elf@users.noreply.github.com>
2025-08-21 16:29:14 +0200

    fix

--- backend/config/initializers/wise.rb
+++ backend/config/initializers/wise.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-WISE_API_KEY = ENV["WISE_API_KEY"]
-WISE_PROFILE_ID = ENV["WISE_PROFILE_ID"]
+WISE_API_KEY = ENV["WISE_API_KEY"].presence || "dummy"
+WISE_PROFILE_ID = ENV["WISE_PROFILE_ID"].presence || "dummy"
 WISE_API_URL = Rails.env.production? ? "https://api.transferwise.com" : "https://api.sandbox.transferwise.tech"
