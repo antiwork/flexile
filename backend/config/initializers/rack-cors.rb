3980ba045ae534d66bf98a14d7d74ccb63a1ddcd
Ershad Kunnakkadan <ershad92@gmail.com>
2025-08-23 11:59:13 -0400

    Refactor

--- backend/config/initializers/rack-cors.rb
+++ backend/config/initializers/rack-cors.rb
@@ -10,7 +10,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
              if: proc { |env| API_DOMAIN == env["HTTP_HOST"] }
   end
   allow do
-    # Allow GETs from the app host (includes port in development)
     origins "#{PROTOCOL}://#{DOMAIN}"
     resource "*", headers: :any, methods: [:get]
   end
