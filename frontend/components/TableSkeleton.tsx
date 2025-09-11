d964b4631ff7459cb4d3cf030f5aad5d84f15e8d
stefan binoj <stefan.binoj.007@gmail.com>
2025-09-11 20:41:18 +0530

    made skeleton responsive

--- frontend/components/TableSkeleton.tsx
+++ frontend/components/TableSkeleton.tsx
@@ -3,21 +3,35 @@ import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
 
 export default function TableSkeleton({ columns }: { columns: number }) {
   return (
-    <Table className="not-print:max-md:grid">
-      <TableBody className="not-print:max-md:contents">
-        {Array.from({ length: 3 }).map((_, rowIndex) => (
-          <TableRow
-            key={rowIndex}
-            className="py-2 not-print:max-md:grid not-print:max-md:grid-cols-1 not-print:max-md:gap-2"
-          >
-            {Array.from({ length: columns }).map((_, colIndex) => (
-              <TableCell key={colIndex} className="px-4 py-2">
-                <Skeleton className={colIndex === columns - 1 ? "h-6 w-24 rounded" : "h-4 w-24 rounded"} />
-              </TableCell>
-            ))}
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+    <>
+      <Table className="hidden md:table">
+        <TableBody>
+          {Array.from({ length: 5 }).map((_, rowIndex) => (
+            <TableRow key={rowIndex}>
+              {Array.from({ length: columns }).map((_, colIndex) => (
+                <TableCell key={colIndex} className="px-4 py-2">
+                  <Skeleton className={colIndex === columns - 1 ? "h-6 w-24 rounded" : "h-4 w-24 rounded"} />
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+
+      <Table className="grid gap-4 md:hidden">
+        <TableBody>
+          {Array.from({ length: 3 }).map((_, rowIndex) => (
+            <TableRow key={rowIndex} className="mb-2 flex flex-col gap-3 rounded-lg p-4">
+              <Skeleton className="h-4 w-32 rounded" /> {/* Title */}
+              <Skeleton className="h-4 w-48 rounded" /> {/* Subtitle */}
+              <div className="flex justify-between">
+                <Skeleton className="h-4 w-20 rounded" /> {/* Left info */}
+                <Skeleton className="h-6 w-16 rounded" /> {/* Button */}
+              </div>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </>
   );
 }
