985b639194f77b695aab793308b6751e810af324
nazar <gladyshinbox@outlook.com>
2025-09-11 16:26:31 +0200

    fix

--- frontend/app/impersonate/page.tsx
+++ frontend/app/impersonate/page.tsx
@@ -2,7 +2,7 @@
 
 import { useSearchParams } from "next/navigation";
 import { useSession } from "next-auth/react";
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
 import PublicLayout from "@/app/(public)/layout";
 
 export default function ImpersonatePage() {
@@ -23,11 +23,13 @@ export default function ImpersonatePage() {
   }, []);
 
   return (
-    <PublicLayout>
-      <div className="flex flex-col items-center rounded-xl bg-white p-8 shadow-lg">
-        <div className="border-muted mb-4 h-8 w-8 animate-spin rounded-full border-4 border-t-black" />
-        <div className="text-md font-semibold">Setting up your session...</div>
-      </div>
-    </PublicLayout>
+    <Suspense>
+      <PublicLayout>
+        <div className="flex flex-col items-center rounded-xl bg-white p-8 shadow-lg">
+          <div className="border-muted mb-4 h-8 w-8 animate-spin rounded-full border-4 border-t-black" />
+          <div className="text-md font-semibold">Setting up your session...</div>
+        </div>
+      </PublicLayout>
+    </Suspense>
   );
 }
