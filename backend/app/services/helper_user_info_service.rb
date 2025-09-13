b60b67b1abe16af00a6fbaad79d72dab59b4cc59
nazar <gladyshinbox@outlook.com>
2025-09-13 17:21:33 +0200

    fix

--- backend/app/services/helper_user_info_service.rb
+++ backend/app/services/helper_user_info_service.rb
@@ -11,9 +11,7 @@ class HelperUserInfoService
 
     {
       name: user.email,
-      actions: {
-        "Impersonate" => user.generate_impersonation_url,
-      },
+      actions: impersonation_actions,
       metadata: {
         "Country of residence" => user.display_country,
         "Contractor for companies" => user.clients.map(&:display_name).to_sentence.presence,
@@ -29,6 +27,12 @@ class HelperUserInfoService
 
   private
     attr_reader :user
+
+    def impersonation_actions
+      return {} if user.team_member?
+
+      { "Impersonate" => user.generate_impersonation_url }
+    end
 
     def investment_notes
       return unless user.investor?
