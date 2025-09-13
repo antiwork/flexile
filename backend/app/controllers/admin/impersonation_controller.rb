35d292ff958cb773feaa03c015051ba46c489666
nazar <gladyshinbox@outlook.com>
2025-09-13 14:15:22 +0200

    fix

--- backend/app/controllers/admin/impersonation_controller.rb
+++ backend/app/controllers/admin/impersonation_controller.rb
@@ -6,8 +6,8 @@ module Admin
       user = User.find_by(email: params[:email])
       return render json: { error_message: "User not found" }, status: :not_found unless user
       return render json: { error_message: "Nice try, but you can't impersonate yourself!" }, status: :unprocessable_entity if user == Current.user
-
       authorize [:admin, :impersonation, user]
+
       render json: { redirect_url: user.generate_impersonation_url }, status: :created
     end
   end
