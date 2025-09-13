317befa68507c91cae0e271ca2a3081bba46d2ab
stefan binoj <stefan.binoj.007@gmail.com>
2025-09-13 10:13:31 +0530

    skeleton fix

--- frontend/components/TableSkeleton.tsx
+++ frontend/components/TableSkeleton.tsx
@@ -1,16 +1,27 @@
 import { Skeleton } from "@/components/ui/skeleton";
 import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
 
-export default function TableSkeleton({ columns }: { columns: number }) {
+interface TableSkeletonProps {
+  columns?: number;
+  hasSelection?: boolean;
+  rows?: number;
+}
+
+export default function TableSkeleton({ columns = 6, hasSelection = false, rows = 5 }: TableSkeletonProps) {
   return (
     <>
       <Table className="hidden md:table">
         <TableBody>
-          {Array.from({ length: 5 }).map((_, rowIndex) => (
+          {Array.from({ length: rows }).map((_, rowIndex) => (
             <TableRow key={rowIndex}>
+              {hasSelection ? (
+                <TableCell className="w-12 min-w-12 py-2">
+                  <Skeleton className="mx-auto h-4 w-4 rounded" />
+                </TableCell>
+              ) : null}
               {Array.from({ length: columns }).map((_, colIndex) => (
-                <TableCell key={colIndex} className="px-4 py-2">
-                  <Skeleton className={colIndex === columns - 1 ? "h-6 w-24 rounded" : "h-4 w-24 rounded"} />
+                <TableCell key={colIndex} className="py-2">
+                  <Skeleton className="h-4 w-20 rounded" />
                 </TableCell>
               ))}
             </TableRow>
@@ -18,20 +29,20 @@ export default function TableSkeleton({ columns }: { columns: number }) {
         </TableBody>
       </Table>
 
-      <Table className="grid gap-4 md:hidden">
-        <TableBody>
+      <div className="grid gap-4 md:hidden">
+        <div>
           {Array.from({ length: 3 }).map((_, rowIndex) => (
-            <TableRow key={rowIndex} className="mb-2 flex flex-col gap-3 rounded-lg p-4">
+            <div key={rowIndex} className="mb-2 flex flex-col gap-3 rounded-lg p-4">
               <Skeleton className="h-4 w-32 rounded" /> {/* Title */}
               <Skeleton className="h-4 w-48 rounded" /> {/* Subtitle */}
               <div className="flex justify-between">
                 <Skeleton className="h-4 w-20 rounded" /> {/* Left info */}
                 <Skeleton className="h-6 w-16 rounded" /> {/* Button */}
               </div>
-            </TableRow>
+            </div>
           ))}
-        </TableBody>
-      </Table>
+        </div>
+      </div>
     </>
   );
 }
