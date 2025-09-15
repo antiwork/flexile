4eef7cf6c3c0becb26046d18ef0e6d72403cf336
nazar <gladyshinbox@outlook.com>
2025-09-15 10:09:06 +0200

    fix

--- frontend/app/(auth)/(credentials)/layout.tsx
+++ frontend/app/(auth)/(credentials)/layout.tsx
@@ -4,7 +4,7 @@ import { redirect, RedirectType, useSearchParams } from "next/navigation";
 import React, { useEffect } from "react";
 import { useUserStore } from "@/global";
 
-export default function AuthLayout({ children }: { children: React.ReactNode }) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   const user = useUserStore((state) => state.user);
   const searchParams = useSearchParams();
 
