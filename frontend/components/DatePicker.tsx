02de961e1e37c9d539c44159ebf3b518951e9985
4arjun <arjunajith440@gmail.com>
2025-09-19 17:07:44 +0530

    remove default values

--- frontend/components/DatePicker.tsx
+++ frontend/components/DatePicker.tsx
@@ -32,11 +32,6 @@ export default function DatePicker({ label, className, ...props }: DatePickerPro
       </div>
       <RacPopover
         placement="bottom end"
-        offset={8}
-        crossOffset={0}
-        shouldFlip
-        shouldUpdatePosition
-        containerPadding={12}
         className="bg-background border-input text-popover-foreground data-entering:animate-in data-exiting:animate-out data-[entering]:fade-in-0 data-[exiting]:fade-out-0 data-[entering]:zoom-in-95 data-[exiting]:zoom-out-95 data-[placement=bottom]:slide-in-from-top-2 data-[placement=left]:slide-in-from-right-2 data-[placement=right]:slide-in-from-left-2 data-[placement=top]:slide-in-from-bottom-2 pointer-events-auto z-50 !transform-none rounded-md border shadow-lg outline-hidden"
       >
         <RacDialog className="max-h-[inherit] overflow-auto p-2">
