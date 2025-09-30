fccba33fa267105b9db5ebf044bad7204b7d1c28
Muhammad Sheik Nauman <noumansheik07@gmail.com>
2025-09-30 22:37:10 +0530

    Fixed issue where blank entries were being saved

--- frontend/app/settings/administrator/page.tsx
+++ frontend/app/settings/administrator/page.tsx
@@ -18,9 +18,9 @@ import { md5Checksum } from "@/utils";
 import StripeMicrodepositVerification from "./StripeMicrodepositVerification";
 
 const formSchema = z.object({
-  website: z.string().url().nullish().or(z.literal("")),
+  website: z.string().trim().min(1, "This field is required.").url("Please enter a valid URL."),
   brandColor: z.string().nullable(),
-  publicName: z.string(),
+  publicName: z.string().trim().min(1, "This field is required"),
 });
 export default function SettingsPage() {
   const company = useCurrentCompany();
