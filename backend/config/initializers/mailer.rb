0df9d2145de7b95f78b9410b69439858d1d58786
John Arndt <johnrarndt@gmail.com>
2025-10-01 19:21:27 -0700

    fix: make RESEND_API_KEY optional for deployment

--- backend/config/initializers/mailer.rb
+++ backend/config/initializers/mailer.rb
@@ -5,5 +5,5 @@ when "test"
   Rails.application.config.action_mailer.delivery_method = :test
 when "development", "staging", "production"
   Rails.application.config.action_mailer.delivery_method = :resend
-  Resend.api_key = ENV.fetch("RESEND_API_KEY")
+  Resend.api_key = ENV["RESEND_API_KEY"] || "dummy_key"
 end
