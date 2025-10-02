fd5a3998415c0e3ac85f58efa63549c77cef469a
abhi <abhiifour@gmail.com>
2025-10-02 11:51:00 +0530

    fix e2e

--- e2e/tests/settings/tax.spec.ts
+++ e2e/tests/settings/tax.spec.ts
@@ -72,7 +72,7 @@ test.describe("Tax settings", () => {
       await page.getByLabel("ZIP code").fill("");
       await page.getByRole("button", { name: "Save changes" }).click();
 
-      await expect(page.getByText("This doesn't look like a complete full name.")).toBeVisible();
+      await expect(page.getByText("Please enter your legal name.")).toBeVisible();
       await expect(page.getByLabel("Tax ID (SSN or ITIN)")).not.toBeValid();
       await expect(page.getByLabel("Residential address (street name, number, apartment)")).not.toBeValid();
       await expect(page.getByLabel("City")).not.toBeValid();
