3110b35ecbf3c4312eeae79d07bb3d83ec5d7968
Smit <smitp.contact@gmail.com>
2025-10-02 18:15:50 +0530

    Address comments 1/2

--- e2e/helpers/matchers.ts
+++ e2e/helpers/matchers.ts
@@ -1,31 +1,29 @@
 import { expect, type Locator, type Page } from "@playwright/test";
-import { assert } from "@/utils/assert";
 
-export const findTableRow = async (page: Page, columnValues: Record<string, string>): Promise<Locator> => {
+export const findTableRow = async (page: Page, columnValues: Record<string, string | RegExp>): Promise<Locator> => {
   const headerRow = page.getByRole("row").first();
-  await expect(headerRow).toBeVisible();
-  const allHeaderTexts = await headerRow.getByRole("columnheader").allTextContents();
-
-  const allHeaderTextToIndex = allHeaderTexts.reduce<Record<string, number>>((map, text, index) => {
-    const trimmedText = text.trim();
-    if (trimmedText) {
-      map[trimmedText] = index;
-    }
-    return map;
-  }, {});
-
-  const columnIndexes = Object.keys(columnValues).reduce<Record<string, number>>((result, label) => {
-    const index = allHeaderTextToIndex[label];
-    assert(index !== undefined, `Could not find column header "${label}" in table.`);
-    result[label] = index;
-    return result;
-  }, {});
-
-  return Object.entries(columnValues).reduce(
-    (rows, [columnLabel, expectedValue]) =>
-      rows.filter({
-        has: page.locator(`td:nth-child(${(columnIndexes[columnLabel] ?? 0) + 1})`, { hasText: expectedValue }),
+  const headerRowByColumnNames = Object.entries(columnValues).reduce(
+    (rowLocator, [columnLabel]) =>
+      rowLocator.filter({
+        has: page.getByRole("columnheader", { name: columnLabel }),
       }),
-    page.locator("tbody tr"),
+    headerRow,
   );
+  await expect(headerRowByColumnNames).toBeVisible();
+
+  const allHeaderTextToIndex = (await headerRow.getByRole("columnheader").allTextContents()).reduce<
+    Map<string, number>
+  >((mappings, text, index) => {
+    const trimmedText = text.trim();
+    if (trimmedText) mappings.set(trimmedText, index);
+    return mappings;
+  }, new Map());
+
+  return Object.entries(columnValues).reduce((rows, [columnLabel, expectedValue]) => {
+    const columnIndex = allHeaderTextToIndex.get(columnLabel);
+    return rows.filter({
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- await expect(headerRowByColumnNames).toBeVisible(); guarantees this never happen
+      has: page.locator(`td:nth-child(${columnIndex! + 1})`, { hasText: expectedValue }),
+    });
+  }, page.locator("tbody tr"));
 };
