762146f7d3536f2064dd17a6363ebd0654053877
Maya <19721695+code-elf@users.noreply.github.com>
2025-10-03 21:40:07 +0200

    fix

--- backend/app/models/invoice.rb
+++ backend/app/models/invoice.rb
@@ -42,6 +42,7 @@ class Invoice < ApplicationRecord
   validates :total_amount_in_usd_cents, presence: true,
                                         numericality: { only_integer: true, greater_than: 99 }
   validates :invoice_number, presence: true
+  validates :bill_from, presence: true
   validates :bill_to, presence: true
   validates :due_on, presence: true
   validates :equity_percentage, presence: true, numericality: {
