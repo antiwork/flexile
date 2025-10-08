f20f758571d1c569868ed95c357be8d5752670ca
nazar <gladyshinbox@outlook.com>
2025-10-08 12:56:50 +0200

    fix

--- frontend/app/(admin)/impersonate/page.tsx
+++ frontend/app/(admin)/impersonate/page.tsx
@@ -2,31 +2,39 @@
 
 import { useRouter, useSearchParams } from "next/navigation";
 import { useSession } from "next-auth/react";
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
 import BrandedLayout from "@/app/(public)/layout";
 
-export default function ImpersonatePage() {
+function ImpersonateHandler() {
   const { data: session, status, update } = useSession();
   const searchParams = useSearchParams();
+  const router = useRouter();
   const param = searchParams.get("actor_token");
   const actorToken = param === "null" ? null : param;
-  const router = useRouter();
 
   useEffect(() => {
     if (status === "loading") return;
 
     const impersonating = actorToken === session?.user.actorToken;
-    if (impersonating) return router.replace("/dashboard");
+    if (!param || impersonating) return router.replace("/dashboard");
 
     void update({ actorToken });
   }, [status, actorToken, session, update]);
 
   return (
-    <BrandedLayout>
-      <div className="bg-background flex flex-col items-center gap-4 rounded-xl p-8 shadow-lg">
-        <div className="border-muted size-8 animate-spin rounded-full border-4 border-t-black dark:border-t-white" />
-        <div className="text-md font-semibold">Setting up your session...</div>
-      </div>
-    </BrandedLayout>
+    <div className="bg-background flex flex-col items-center gap-4 rounded-xl p-8 shadow-lg">
+      <div className="border-muted size-8 animate-spin rounded-full border-4 border-t-black dark:border-t-white" />
+      <div className="text-md font-semibold">Setting up your session...</div>
+    </div>
+  );
+}
+
+export default function ImpersonatePage() {
+  return (
+    <Suspense>
+      <BrandedLayout>
+        <ImpersonateHandler />
+      </BrandedLayout>
+    </Suspense>
   );
 }
