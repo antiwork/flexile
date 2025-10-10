c76b3aa1fc3b1de8c8b6722ff4613bd46ad3d940
kanhaya kumar yadav <kanhaya.workspace@gmail.com>
2025-10-10 09:24:13 +0530

    add test for job and remove skip_pdf_generation

--- backend/spec/services/equity_exercising_service_spec.rb
+++ backend/spec/services/equity_exercising_service_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-RSpec.describe EquityExercisingService, :skip_pdf_generation do
+RSpec.describe EquityExercisingService do
   let!(:company) { create(:company, :completed_onboarding, name: "Gumroad") }
   let!(:exercise_notice) { create(:document_template, :exercise_notice, company:) }
   let(:user) { create(:user) }
@@ -109,6 +109,8 @@ RSpec.describe EquityExercisingService, :skip_pdf_generation do
       expect(exercise.total_cost_cents).to eq((equity_grant.exercise_price_usd * 100 * 100).round)
       expect(exercise.status).to eq(EquityGrantExercise::SIGNED)
       expect(exercise.bank_reference).to eq(equity_grant.name)
+
+      expect(CreateDocumentPdfJob).to have_enqueued_sidekiq_job.once
     end
 
     context "when there is no exercise notice" do
