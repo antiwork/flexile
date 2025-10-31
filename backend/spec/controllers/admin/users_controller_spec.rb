b30ec89d6136503c1ec7c829d51d21df28ed2274
nazar <gladyshinbox@outlook.com>
2025-10-31 02:30:01 +0100

    fix

--- backend/spec/controllers/admin/users_controller_spec.rb
+++ backend/spec/controllers/admin/users_controller_spec.rb
@@ -14,26 +14,49 @@ RSpec.describe Admin::UsersController do
     end
   end
 
+  def expect_impersonate(authenticated_user:, user:)
+    service = ImpersonationService.new(authenticated_user)
+
+    expect(ImpersonationService).to receive(:new).with(authenticated_user).and_return(service)
+    expect(service).to receive(:impersonate).with(user).and_call_original
+  end
+
+  def expect_unimpersonate(authenticated_user:)
+    service = ImpersonationService.new(authenticated_user)
+
+    expect(ImpersonationService).to receive(:new).with(authenticated_user).and_return(service)
+    expect(service).to receive(:unimpersonate).and_call_original
+  end
+
+  def expect_no_service_call
+    expect(ImpersonationService).not_to receive(:new)
+  end
+
   describe "GET #impersonate" do
     it "allows impersonating regular users" do
+      expect_impersonate(authenticated_user: team_member_user, user: user)
+
       get :impersonate, params: { id: user.external_id }
 
       expect(response).to redirect_to(dashboard_path)
-      expect($redis.get(RedisKey.impersonated_user(team_member_user.id))).to eq(user.id.to_s)
     end
 
     it "denies impersonating other team members" do
+      expect_no_service_call
+
       get :impersonate, params: { id: another_team_member_user.external_id }
 
       expect(response).to redirect_to(admin_users_path)
-      expect($redis.get(RedisKey.impersonated_user(team_member_user.id))).to be_nil
+      expect(flash[:alert]).to eq("The requested resource could not be accessed.")
     end
 
     it "denies impersonating non-existent users" do
+      expect_no_service_call
+
       get :impersonate, params: { id: "non-existent-external-id" }
 
       expect(response).to redirect_to(admin_users_path)
-      expect($redis.get(RedisKey.impersonated_user(team_member_user.id))).to be_nil
+      expect(flash[:alert]).to eq("The requested resource could not be accessed.")
     end
 
     context "when current user is not a team member" do
@@ -45,10 +68,11 @@ RSpec.describe Admin::UsersController do
       end
 
       it "denies impersonation of any user" do
+        expect_no_service_call
+
         get :impersonate, params: { id: user.external_id }
 
         expect(response).to redirect_to(dashboard_path)
-        expect($redis.get(RedisKey.impersonated_user(non_team_member.id))).to be_nil
       end
     end
   end
@@ -57,10 +81,11 @@ RSpec.describe Admin::UsersController do
     before { get :impersonate, params: { id: user.external_id } }
 
     it "ends impersonation session" do
+      expect_unimpersonate(authenticated_user: team_member_user)
+
       delete :unimpersonate
 
       expect(response.parsed_body[:success]).to be true
-      expect($redis.get(RedisKey.impersonated_user(team_member_user.id))).to be_nil
     end
   end
 end
