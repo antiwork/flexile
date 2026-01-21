b562e581612032be8376ef512f2a55a3a10c3001
Ershad Kunnakkadan <ershad92@gmail.com>
2026-01-20 20:21:15 -0500

    refactor: remove sender name from company update email footer (#1555)

--- e2e/tests/company/updates/company/view-updates.spec.ts
+++ e2e/tests/company/updates/company/view-updates.spec.ts
@@ -10,14 +10,12 @@ import { withinModal } from "@test/index";
 
 test.describe("view company updates", () => {
   let company: Awaited<ReturnType<typeof companiesFactory.createCompletedOnboarding>>["company"];
-  let adminUser: Awaited<ReturnType<typeof companiesFactory.createCompletedOnboarding>>["adminUser"];
   let user: Awaited<ReturnType<typeof usersFactory.create>>["user"];
-  const adminUserpreferredName = "Test Admin";
 
   test.beforeEach(async () => {
     const result = await companiesFactory.create();
     company = result.company;
-    adminUser = (await usersFactory.create({ preferredName: adminUserpreferredName })).user;
+    const adminUser = (await usersFactory.create()).user;
     await companyAdministratorsFactory.create({
       companyId: company.id,
       userId: adminUser.id,
@@ -44,7 +42,6 @@ test.describe("view company updates", () => {
     await withinModal(
       async (modal) => {
         await expect(modal.getByText("Test contractor view content for body.")).toBeVisible();
-        await expect(modal.getByText(adminUserpreferredName)).toBeVisible();
       },
       { page, title: companyUpdate.title, assertClosed: false },
     );
@@ -65,7 +62,6 @@ test.describe("view company updates", () => {
     await withinModal(
       async (modal) => {
         await expect(modal.getByText("Test investor view content for body.")).toBeVisible();
-        await expect(modal.getByText(adminUserpreferredName)).toBeVisible();
       },
       { page, title: companyUpdate.title, assertClosed: false },
     );
